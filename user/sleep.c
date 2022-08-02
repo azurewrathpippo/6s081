2ba64954bc07faec7de5973d6d7069aa0e16fc0e
Hongjun Zhu <782305756@qq.com>
2022-08-03 02:32:56 +0800

    finish sleep command

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int t;
+
+  if(argc < 2){
+    fprintf(2, "Usage: sleep for times...\n");
+    exit(1);
+  }
+
+  t = atoi(argv[1]);
+  sleep(t);
+
+  exit(0);
+}
